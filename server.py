024a292b304725c6649cba92c57da115329842b7
ddiddi <ddiddi>
2024-07-02 23:25:34 -0700

    feat: add local storage llamaindex rag

--- server.py
+++ server.py
@@ -1,10 +1,53 @@
 from fastapi import FastAPI, Request, Response
 import httpx
 from starlette.responses import StreamingResponse
+from llama_index.core import Settings
+from llama_index.embeddings.llamafile import LlamafileEmbedding
+from llama_index.llms.llamafile import Llamafile
+from llama_index.core.node_parser import SentenceSplitter
+from llama_index.core import SimpleDirectoryReader, VectorStoreIndex
+from llama_index.readers.web import SimpleWebPageReader
 
 app = FastAPI()
 
 llama_cpp_url = "http://localhost:8080"
+
+# Based on https://www.llamaindex.ai/blog/using-llamaindex-and-llamafile-to-build-a-local-private-research-assistant
+# Configure LlamaIndex
+
+def load_local_data(input_dir='./data'):
+    local_doc_reader = SimpleDirectoryReader(input_dir=input_dir)
+    return local_doc_reader.load_data(show_progress=True)
+
+def load_web_data(urls):
+    web_reader = SimpleWebPageReader(html_to_text=True)
+    return web_reader.load_data(urls)
+
+def build_index(docs, persist_dir="./storage"):
+    index = VectorStoreIndex.from_documents(docs, show_progress=True)
+    index.storage_context.persist(persist_dir=persist_dir)
+    return index
+
+def configure_llama_index():
+    Settings.embed_model = LlamafileEmbedding(base_url=llama_cpp_url)
+    Settings.llm = Llamafile(
+        base_url=llama_cpp_url,
+        temperature=0,
+        seed=0
+    )
+    Settings.transformations = [
+        SentenceSplitter(
+            chunk_size=256,
+            chunk_overlap=5
+        )
+    ]
+
+def query_rag(index, question):
+    query_engine = index.as_query_engine()
+    response = query_engine.query(question)
+    return response
+
+# TODO: add mongo store: https://docs.llamaindex.ai/en/stable/examples/vector_stores/MongoDBAtlasVectorSearch/
 
 @app.post("/stream")
 async def stream_to_llama_cpp(request: Request):
@@ -26,7 +69,6 @@ async def stream_to_llama_cpp(request: Request):
 @app.post("/completion")
 async def completion(request: Request):
     client_request_data = await request.body()
-    print("something happen")
     async def event_generator():
         try:
             async with httpx.AsyncClient() as client:
@@ -47,4 +89,15 @@ async def completion(request: Request):
 
 if __name__ == "__main__":
     import uvicorn
+    # Load data and build index
+    local_docs = load_local_data()
+    web_docs = load_web_data([
+        'https://en.wikipedia.org/wiki/Homing_pigeon',
+        'https://en.wikipedia.org/wiki/Magnetoreception'
+    ])
+    all_docs = local_docs + web_docs
+    build_index(all_docs)
+    
+    # Configure LlamaIndex
+    configure_llama_index()
     uvicorn.run(app, host="0.0.0.0", port=8000)
